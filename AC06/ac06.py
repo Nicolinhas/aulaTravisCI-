674a4ba8c040a6950d971df43d8ef441d3b12629
nicolas lee <nicolas.lee@aluno.faculdadeimpacta.com.br>
2019-10-04 10:26:43 -0300

    ac06

---
+++ AC06/ac06.py
@@ -0,0 +1,19 @@
+class funcionario:
+	.def __init__(self,)
+
+class diciplina:
+	.def __init__(self,)
+
+class administrativo:
+	.def __init__(self,)
+
+class professor:
+	.def __init__(self,)
+
+
+class aluno:
+	.def __init__(self,)
+
+
+class curso:
+	.def __init__(self,)
